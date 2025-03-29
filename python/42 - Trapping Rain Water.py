0a0c7194ecac309e811933a7ffa2aae8a03383e1
Roger Donegati <roger.donegati@gmail.com>
2025-03-29 18:10:50 -0300

    fix: enhanced challenge solution

--- python/42 - Trapping Rain Water.py
+++ python/42 - Trapping Rain Water.py
@@ -1,42 +1,66 @@
-# TODO there is clearly room for improvement here....
 
 def trap(height: list[int]) -> int:
     result = 0
     left = {
         "value": height[0],
         "index": 0,
-        "sum": 0,
+        "heightArea": 0,
+    }
+    right = {
+        "value": height[len(height)-1],
+        "index": 0,
+        "heightArea": 0,
     }
 
     for i in range(1,len(height)):
         if left['value'] <= height[i]:
-            result += (i - left['index'] - 1) * left['value'] - left['sum']
+            result += (i - left['index'] - 1) * left['value'] - left['heightArea']
             left['value'] = height[i]
-            left['sum'] = 0
+            left['heightArea'] = 0
             left['index'] = i
         else:
-            left['sum'] += height[i]
+            left['heightArea'] += height[i]
 
-    right = {
-        "value": height[len(height)-1],
-        "index": len(height)-1,
-        "sum": 0,
-    }
-    for i in range(len(height) -2, -1, -1):
-        if right['value'] <= height[i]:
-            result += (right['index'] - i -1) * right['value'] - right['sum']
-            right['value'] = height[i]
-            right['sum'] = 0
+    newHeight = height[left['index']-len(height):][::-1]
+    for i in range(1, len(newHeight)):
+        if right['value'] <= newHeight[i]:
+            result += (i - right['index'] - 1) * right['value'] - right['heightArea']
+            right['value'] = newHeight[i]
+            right['heightArea'] = 0
             right['index'] = i
         else:
-            right['sum'] += height[i]
-        if (height[i] == left['value'] or right['value'] == left['value'] ) and result >0:
+            right['heightArea'] += newHeight[i]
+        if (newHeight[i] == left['value'] or right['value'] == left['value'] ) and result >0:
             break
     return result
 
-print(trap([6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3])) # 81
-print(trap([0,1,0,2,1,0,1,3,2,1,2,1])) # 6
-print(trap([4,2,0,3,2,4,3,4])) # 10
-print(trap([4,2,0,3,2,5])) # 9
-print(trap([4,2,3])) # 1
-print(trap([2,0,2])) # 2
+def optimzedTrap(height: list[int]) -> int:
+    left = 0
+    right = len(height) - 1
+    left_max = height[left]
+    right_max = height[right]
+    water = 0
+
+    while left < right:
+        if left_max < right_max:
+            # this means that there is a wall in right higher than any wall in the left
+            # so we just need to calcula max left - current height
+            left += 1
+            left_max = max(left_max, height[left])
+            water += left_max - height[left]
+        else:
+            # this means that there is a wall in left higher than any wall in the right
+            # so we just need to calcula max right - current height
+            right -= 1
+            right_max = max(right_max, height[right])
+            water += right_max - height[right]
+    return water
+
+
+
+print(optimzedTrap([6,4,2,0,3,2,0,3,1,4,5,3,2,7,5,3,0,1,2,1,3,4,6,8,1,3])) # 83
+print(optimzedTrap([0,1,0,2,1,0,1,3,2,1,2,1])) # 6
+print(optimzedTrap([4,2,0,3,2,4,3,4])) # 10
+print(optimzedTrap([4,2,0,3,2,5])) # 9
+print(optimzedTrap([4,2,3])) # 1
+print(optimzedTrap([2,0,2])) # 2
