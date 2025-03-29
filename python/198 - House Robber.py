2bce8bd87688ccfe95efe5e7322cd91cc0c69963
Roger Donegati <roger.donegati@gmail.com>
2025-03-28 22:58:34 -0300

    chore: included validation values

--- python/198 - House Robber.py
+++ python/198 - House Robber.py
@@ -1,4 +1,4 @@
-def rob( nums: list[int]) -> int:
+def rob(nums: list[int]) -> int:
     x, y = 0, 0
     for num in nums:
         if num + y > x:
@@ -7,3 +7,7 @@ def rob( nums: list[int]) -> int:
             x, y = x, x
     
     return x
+
+
+print(rob([1,2,3,1]))   # 4
+print(rob([2,7,9,3,1])) # 12
